54ba39e4449cb60f11288405ab94264ea574742a
David Costa <davidcostadev@gmail.com>
2019-03-30 20:12:40 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   plugins: ['react', 'prettier'],
   extends: ['airbnb', 'prettier'],
   rules: {
+    'react/forbid-prop-types': ['error', { forbid: ['object', 'array'] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'prettier/prettier': ['error'],
   },
   env: { es6: true },
